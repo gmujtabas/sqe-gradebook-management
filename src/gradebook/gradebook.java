eec9b07af82c5d62c717165485e2e9676c8ac8fb
Ghulam Mujtaba <mujtabamaharr@gmail.com>
2026-08-21 00:02:32 +0500

    chore: initial project structure

--- src/gradebook/gradebook.java
+++ src/gradebook/gradebook.java
@@ -1,1 +1,13 @@
+import java.util.ArrayList;
 
+public class Student {
+    String name;
+    String rollNo;
+    ArrayList<Integer> scores;
+
+    public Student(String name,String rollNo) {
+        this.name=name;
+        this.rollNo=rollNo;
+        this.scores=new ArrayList<>();
+    }
+}
