31bf7cb133afd61d47d2268f4fe0eb4e65df02b7
Ghulam Mujtaba <mujtabamaharr@gmail.com>
2026-08-23 21:46:36 +0500

    feat: validate student score

--- src/gradebook/Student.java
+++ src/gradebook/Student.java
@@ -23,6 +23,9 @@ public class Student {
     }
 
     public void addScore(int score) {
+        if (score < 0) {
+            throw new IllegalArgumentException("Score cannot be negative");
+        }
         scores.add(score);
     }
 }
