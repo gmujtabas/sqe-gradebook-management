cec5c85b6ee0b2ac0b9eb123fcfca9fe3cb8dfd3
Ghulam Mujtaba <mujtabamaharr@gmail.com>
2026-09-13 23:54:24 +0500

    Reorganized project directories and updated files

--- src/main/java/gradebook/gradebook.java
+++ src/main/java/gradebook/gradebook.java
@@ -55,4 +55,22 @@ public class gradebook {
 
         input.close();
     }
+
+    public static char letterGrade(int score) {
+    if (score < 0 || score > 100) {
+        throw new IllegalArgumentException("Score must be between 0 and 100");
+        }
+
+        if (score >= 90) {
+            return 'A';
+        } else if (score >= 80) {
+            return 'B';
+        } else if (score >= 70) {
+            return 'C';
+        } else if (score >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
 }
