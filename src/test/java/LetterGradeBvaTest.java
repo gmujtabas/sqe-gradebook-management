e963ff1f763c2761a9000891ec8515c7feba4656
Ghulam Mujtaba <mujtabamaharr@gmail.com>
2026-09-15 16:41:06 +0500

    updated LetterGradeBVATest

--- src/test/java/LetterGradeBvaTest.java
+++ src/test/java/LetterGradeBvaTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class LetterGradeBvaTest {
+public class LetterGradeBVATest {
 
     @ParameterizedTest
     @CsvSource({
