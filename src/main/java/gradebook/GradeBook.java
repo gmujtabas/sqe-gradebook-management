cdfab39c21eccc7eaeb524fd06fc25c8e393a7f6
Ghulam Mujtaba <mujtabamaharr@gmail.com>
2026-09-23 23:57:03 +0500

    Rename gradebook.java to GradeBook.java

--- src/main/java/gradebook/GradeBook.java
+++ src/main/java/gradebook/GradeBook.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class gradebook {
+public class GradeBook {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         ArrayList<Student> students = new ArrayList<>();
